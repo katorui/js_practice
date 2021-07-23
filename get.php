165a6dc077808d83868c011011ea9fc4c17ba1ff
加藤瑠依 <rui14jpn@gmail.com>
2021-07-23 10:16:07 +0900

    change

--- get.php
+++ get.php
@@ -1,4 +1,25 @@
 <?php
 
-echo "成功";
-echo 'img/pien_woman.png' . $_GET['name']. $_GET['age'];
+$data = [
+    'id' => 1,
+    'name' => $_GET['name'],
+    'age' => $_GET['age'],
+    'img' => 'img/boy_sad.png'
+];
+// 宿題2 ブラウザに表示
+$json = json_encode(
+    $data,
+    JSON_UNESCAPED_SLASHES
+    | JSON_UNESCAPED_UNICODE
+);
+// ヘッダーにJSONで返すことを伝える
+header("Content-Type: application/json");
+echo $json;
+
+// $user = '{
+//             "id" : 1,
+//             "name" : "ペティス",
+//             "age" : 26
+//         }';
+
+// echo $user;
