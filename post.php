c01d5ee8738164c705d76ecf4935d223e4cf4e36
加藤瑠依 <rui14jpn@gmail.com>
2021-07-25 19:57:11 +0900

    ajax_post

--- post.php
+++ post.php
@@ -1,3 +1,15 @@
 <?php
 
-echo "POST通信です" . $_POST["name"];
+$data = [
+    'img' => $_POST['img'],
+    'name' => $_POST['name']
+];
+// 宿題2 ブラウザに表示
+$json = json_encode(
+    $data,
+    JSON_UNESCAPED_SLASHES
+    | JSON_UNESCAPED_UNICODE
+);
+// ヘッダーにJSONで返すことを伝える
+header("Content-Type: application/json");
+echo $json;
